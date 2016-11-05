d3c26176c27026fa95a47e53c1f556735450b1d6
craig91 <craigdejean1@gmail.com>
2016-11-04 22:15:53 -0400

    Java Lesson 1

---
+++ JavaLesson#1.java
@@ -0,0 +1,13 @@
+public class Main{ // <=== This is the name of the class/program
+    public static void main(string[] args){ // <== this is the main method
+        System.out.println("Welcome to Java");
+        System.out.println("Let's demonstrate a simple calculation");
+        int answer;
+        answer = 2 + 2;
+        System.out.println("2 plus 2 is " + answer);
+    }
+}
+
+
+// System.out is an object that is used for sending output to the screen 
+// println is the method(action) that this object performs (printing the args to the screen)
