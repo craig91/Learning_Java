024a92a9b6e3d5132d7c41e79c8be49b78960edb
craig91 <craigdejean1@gmail.com>
2016-11-04 22:29:15 -0400

    Self test excerise #1

---
+++ Self_test_1.java
@@ -0,0 +1,25 @@
+// Self Test excercises
+
+// 1. If the following statement were used in a java program, it would cause something
+// to be written to the screen. What would it cause to be written to the screen?
+
+System.out.println("Java is not a drink.");
+
+// Answer: This will pring Java is not a drink to the screen
+
+// 2. Give a statement or statements that can be used in a Java program to write the following to the screen
+// I like Java
+// You like tea
+
+        System.out.println("I like Java");
+        System.out.println("You like tea");
+
+// 3. Write a complete Java program that uses System.out.println to output the following to the screen when run:
+// Hello world!
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
+
