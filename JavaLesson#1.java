cad90e9e05f9163ebbe52a75b6c7c51884322683
craig91 <craigdejean1@gmail.com>
2016-12-19 12:13:11 -0800

    added some more tips

--- JavaLesson#1.java
+++ JavaLesson#1.java
@@ -2,8 +2,8 @@ public class Main{ // <=== This is the name of the class/program
     public static void main(String[] args){ // <== this is the main method
         System.out.println("Welcome to Java");
         System.out.println("Let's demonstrate a simple calculation");
-        int answer;
-        answer = 2 + 2;
+        int answer; // <=== variable declaration 
+        answer = 2 + 2; // <=== variable assignment ( answer is NOT equal to 2 + 2, the equal sign is saying to make answer equal to 2 + 2)
         System.out.println("2 plus 2 is " + answer);
     }
 }
@@ -11,3 +11,6 @@ public class Main{ // <=== This is the name of the class/program
 
 // System.out is an object that is used for sending output to the screen 
 // println is the method(action) that this object performs (printing the args to the screen)
+
+// int answer =  int is one of java's data types for integers (whole numbers); This line says that answer is a variable that can hold
+// a single integer. 
