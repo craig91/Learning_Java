e83dc75caebc1b42f82b4a45831715202dc023ff
craig91 <craigdejean1@gmail.com>
2016-12-19 11:27:14 -0800

    added basic java structure

---
+++ java_basic_structure.java
@@ -0,0 +1,7 @@
+public class FirstProgram {  // <==== This is saying that this is a program named FirstProgram
+
+    public static void main(String[] args){ // <=== This is the method main, it is invoked when FirstProgram is run, 
+                                                  //anything inside this method will run when FirstProgram is called
+
+    }
+}
