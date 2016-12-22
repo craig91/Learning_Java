bc588e047fe8abbe18b3ccb3415d2e6112713d01
craig91 <craigdejean1@gmail.com>
2016-12-21 23:16:47 -0500

    java variable explanation

---
+++ java_vars.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int numberOfBeans;  // <-- this variable declaration numberOfBeans can hold the value type of int (aka a whole number)
+                            // the type int is default type for whole numbers. 
+        double oneWeight, totalWeight; // <-- this declaration has the variable type of double, which is the default type for numbers 
+    }                                  // with a decimal point(known also as floating-point numbers)
+}
+
