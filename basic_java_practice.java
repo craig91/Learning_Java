e342b640b9ef37bc043b1778fddbd169ef97d19d
craig91 <craigdejean1@gmail.com>
2016-12-21 23:36:33 -0500

    Java practice file

---
+++ basic_java_practice.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        int oneWeight, numberOfBeans, totalWeight;
+        oneWeight = 5;
+        numberOfBeans = 100;
+        totalWeight = oneWeight * numberOfBeans;
+        System.out.println("The total weight is: " + totalWeight);
+    }
+}
