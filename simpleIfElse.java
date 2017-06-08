bc6ff305da801bdd777aac0547af5f67f4be9043
craig91 <craigdejean1@gmail.com>
2017-06-07 20:32:58 -0400

    simpler version of if else conditional

---
+++ simpleIfElse.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    v1 = 10;
+    v2 = 4;
+
+    if(v1 > v2)
+        System.out.println("V1 is bigger");
+    else
+        System.out.println("V2 is NOT bigger");
+  }
+}
