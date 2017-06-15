2e275382d4460599dc803f9431d62b053f57f678
Craig Dejean <thelegendarycraig@yahoo.com>
2017-06-15 00:22:14 -0400

    updated calulator using parallel arrays & loops

--- calcEngine.java
+++ calcEngine.java
@@ -1,31 +1,49 @@
-package com.craig.calcengine;
+package com.craig.calculator;
+
 public class Main {
-  public static void main(String[] args) {
 
-    double val1 = 100.0d;
-    double val2 = 50.0d;
+    public static void main(String[] args) {
 
-    double result;
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};  // ----> These 4 arrays are called Parallel Arrays
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};   // based on the length, each value works together ex: 100.0d -> 50.0d -> 'd' -> new double[]
+        char[] opCodes = {'d', 'a', 's', 'm'};
+        double[] results = new double[opCodes.length];
 
-    char opCode = "a";
 
-    if(opCode == 'a')
-        result = val1 + val2;
-    else if(opCode == 's')
-        result = val1 - val2;
-    else if(opCode == 'm')
-        result = val1 * val2;
-    else if(opCode == 'd'){ // <=== turned into a block statement for readablility
-      result = val2 != 0.0d ? val1 / val2 : 0.0d;
-      // if(val2 != 0.0d)  <======  2 different conditionals for the same purpose.
-      //    result = val1 / val2;
-      // else
-      //    result = 0.0d;
+//        double val1 = 100.0d;
+//        double val2 = 50.0d;
+//        double result;
+//        char opCode = 'd';
+
+        for(int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                results[i] = leftVals[i] + rightVals[i];
+                break;
+
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+
+                case 'd':
+                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0d;
+                    break;
+
+            }
+        }
+        for(double theResult : results) {
+            System.out.println("Result =");
+            System.out.println(theResult);
+        }
+
+
+
     }
-        result = val1 / val2;
-    else
-        result = 0.0d;
-
-    System.out.println(result);
-  }
 }
