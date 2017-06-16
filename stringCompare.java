7fe4ff0e1d8e94d171fe802ed3599869b7d2a33b
Craig Dejean <thelegendarycraig@yahoo.com>
2017-06-15 23:26:31 -0400

    comments

---
+++ stringCompare.java
@@ -0,0 +1,20 @@
+package com.craig.stringComp;
+
+public class Main{
+   public static void main(String[] args) {
+    String s1 = "hi";
+    String s2 = "Hi";
+
+    // if(s1.equalsIgnoreCase(s2))
+    //    System.out.println("These are equal strings");
+    // else
+    //    System.out.println("These are not equal strings");
+
+    if(s1.equals(s2))
+       System.out.println("These are equal strings");
+    else
+       System.out.println("These are not equal strings");
+
+
+  }
+}
